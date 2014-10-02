3ae058cd2911d36d43771ced5ddda64fddc50cca
Stanislav Shabalin <services@voxpuibr.com>
2014-10-02 14:22:31 +0600

    Saving a single to-do item (tests + implementation)

--- lists/tests.py
+++ lists/tests.py
@@ -17,3 +17,19 @@ class HomePageTest(TestCase):
         response = home_page(request)
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        item_text = 'A new list item'
+
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = item_text
+
+        response = home_page(request)
+        
+        self.assertIn(item_text, response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': item_text}
+        )
+        self.assertEqual(response.content.decode(), expected_html)
